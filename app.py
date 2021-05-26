702e74a9340cfa86ae89c638182e42588b479bc7
deusr3x <ahoare@gmail.com>
2021-05-26 20:53:12 +1000

    added list view

--- app.py
+++ app.py
@@ -33,5 +33,11 @@ def create():
         return redirect('/data')
 
 
+@app.route('/data')
+def RetrieveList():
+    inv = InventoryModel.query.all()
+    return render_template('datalist.html', inv=inv)
+
+
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
